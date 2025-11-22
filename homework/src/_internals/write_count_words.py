95ea1adb12cafe27cb6f1dbb7d531d32533e52cf
jarbelaezu98 <jarbelaezu@unal.edu.co>
2025-11-22 10:10:50 -0500

    Variables de carpetas

--- homework/src/_internals/write_count_words.py
+++ homework/src/_internals/write_count_words.py
@@ -1,10 +1,10 @@
 import os
 
 
-def write_count_words(counter):
+def write_count_words(counter, output_folder):
     # create the directory output/ if it doesn't exist
-    if not os.path.exists("data/output"):
-        os.makedirs("data/output")
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
     # save the results using tsv format
     with open("data/output/results.tsv", "w", encoding="utf-8") as f:
